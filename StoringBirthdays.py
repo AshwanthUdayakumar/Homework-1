1abef99d12111436c554e54c8967f62e8d61c9a3
AshwanthUdayakumar <ashwanthudayakumar15@gmail.com>
2026-05-08 10:04:49 +0400

    Homework

---
+++ StoringBirthdays.py
@@ -0,0 +1,13 @@
+# Creating different variables
+y = "John"
+x = 15
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
